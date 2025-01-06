8ce293e3fc45aa1cf71482a1d476c5c1c65795dc
Frederic Marechal <freddy.marechal@gmail.com>
2025-01-06 08:42:26 +0000

    upgrading to python 3.13

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ with open('requirements.txt') as f:
 
 setup(
     name="data_providers",
-    version="0.0.3",
+    version="0.0.5",
     include_package_data=True,
-    python_requires='>=3.8',
+    python_requires='>=3.13',
     packages=find_packages(),
     setup_requires=['setuptools-git-versioning'],
     install_requires=requirements,
@@ -17,7 +17,7 @@ setup(
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     classifiers=[
-        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.13",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
