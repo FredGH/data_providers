171125b7a4ef8ffdafeff66eb287165b1ca0ae0f
Frederic Marechal <freddy.marechal@gmail.com>
2025-01-09 08:32:11 +0000

    wip

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name="data_providers",
-    version="0.0.5",
+    version="0.0.6",
     include_package_data=True,
     python_requires='>=3.13',
     packages=find_packages(),
